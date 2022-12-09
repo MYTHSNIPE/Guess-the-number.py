38dac0b074e2447b998b972a9cd446e9aad1607d
MYTHSNIPE <103127423+MYTHSNIPE@users.noreply.github.com>
2022-12-09 23:34:32 +0530

    Update guess_the_nme.py

--- guess_the_nme.py
+++ guess_the_nme.py
@@ -1,26 +1,54 @@
 import random
 print("RULE TO PLAY THIS GAME:\n1) YOU WILL GET ONLY NINE CHANCES TO GUESS\n2) IF YOU TRIED CHEATING(LIKE MESSING UP WITH CODE YOU WILL BE DISQUALIFIED)")
 while(True):
-    i=1
-    difficulty=(input("enter the difficulty(easy,medium,hard): "))
-    difficulty=difficulty.lower()
-    if difficulty=="easy":
-        number=random.randint(0,100)
-        attempts=12
-    elif difficulty=="medium":
-        number=random.randint(0,200)
-        attempts=9
-    elif difficulty=="hard":
-        number=random.randint(0,500)
-        attempts=7
-    value=int(input("enter the number: "))
+    while True:
+            i=1
+            difficulty=(input("enter the difficulty(easy,medium,hard): "))
+            difficulty=difficulty.lower()
+            if difficulty=="easy":
+                number=random.randint(0,100)
+                attempts=12
+                break
+            elif difficulty=="medium":
+                number=random.randint(0,200)
+                attempts=9
+                break
+            elif difficulty=="hard":
+                number=random.randint(0,500)
+                attempts=7
+                break
+            else:
+                print("please enter valid input")
+    while i<=attempts:
+        try:
+            value=int(input("enter the number: "))
+            break
+        except Exception as e:
+            print("You loose one attempt for invalid input")
+            i=i+1
+    if i>=attempts:
+        print("Unfortunately you lost")
+        print("enter 1 to try again or any other key to exit: ")
+        repeater=input()
+        if repeater=="1":
+            continue
+        else :
+            break           
     while(value!=number):
         if value>number:
-            value=int(input("Try a smaller number: "))
+            try:
+                value=int(input("Try a smaller number: "))
+            except Exception as e:
+                print("You loose one attempt for invalid input")
+                i=i+1
         elif value<number:
-            value=int(input("Try a larger value: "))
+            try:
+                value=int(input("Try a larger value: "))
+            except Exception as e:
+                print("You loose one attempt for invalid input")
+                i=i+1
         i=i+1
-        if i==attempts:
+        if i>=attempts:
             print("Unfortunately you lost :( ")
             break
     if i!=attempts:
