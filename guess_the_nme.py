9d36ec3114c217b1fb813debfd49c497ae1f517f
MYTHSNIPE <103127423+MYTHSNIPE@users.noreply.github.com>
2022-12-09 22:04:17 +0530

    Update guess_the_nme.py

--- guess_the_nme.py
+++ guess_the_nme.py
@@ -1,14 +1,18 @@
 import random
-i=0
-difficulty=(input("enter the difficulty(easy,medium,hard): "))
-difficulty=difficulty.lower()
-if difficulty=="easy":
-    number=random.randint(0,100)
-elif difficulty=="medium":
-    number=random.randint(0,200)
-elif difficulty=="hard":
-    number=random.randint(0,500)
+print("RULE TO PLAY THIS GAME:\n1) YOU WILL GET ONLY NINE CHANCES TO GUESS\n2) IF YOU TRIED CHEATING(LIKE MESSING UP WITH CODE YOU WILL BE DISQUALIFIED)")
 while(True):
+    i=1
+    difficulty=(input("enter the difficulty(easy,medium,hard): "))
+    difficulty=difficulty.lower()
+    if difficulty=="easy":
+        number=random.randint(0,100)
+        attempts=12
+    elif difficulty=="medium":
+        number=random.randint(0,200)
+        attempts=9
+    elif difficulty=="hard":
+        number=random.randint(0,500)
+        attempts=7
     value=int(input("enter the number: "))
     while(value!=number):
         if value>number:
@@ -16,9 +20,20 @@ while(True):
         elif value<number:
             value=int(input("Try a larger value: "))
         i=i+1
-    print("Booooooom You guessed it correct in",i,"turns\n\t enter 1 if you want to continue else enter any other key to exit: ")
-    repeater=input()
-    if repeater=="1":
-        continue
-    else :
-        break
+        if i==attempts:
+            print("Unfortunately you lost :( ")
+            break
+    if i!=attempts:
+        print("Booooooom You guessed it correct in",i,"turns\n\t enter 1 if you want to continue else enter any other key to exit: ")
+        repeater=input()
+        if repeater=="1":
+            continue
+        else :
+            break
+    else:
+        print("enter 1 to try again or any other key to exit: ")
+        repeater=input()
+        if repeater=="1":
+            continue
+        else :
+            break
